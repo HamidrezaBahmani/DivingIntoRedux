f3b3d7ec8c5921f07623006dc000939b17eeb605
HamidrezaBahmani <hb.hamidrezabahmani.com@gmail.com>
2023-11-27 21:49:00 +0330

    Creating a Redux Store for React

---
+++ src/store/index.js
@@ -0,0 +1,31 @@
+//1-for deploying redux first make store folder
+//2-in that make file named index.js
+
+//3- import createStore from redux
+import { createStore } from "redux";
+
+//5- we need reducer pointer for createStore()
+//state={counter:0} =>default value
+//here we handle diffrent states
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increament") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+
+  return state;
+};
+
+//4-const store = createStore();
+//6-const store = createStore(counterReducer);
+const store = createStore(counterReducer);
+
+//7- now we make this redux store we should make it
+//available for other component
+export default store;
