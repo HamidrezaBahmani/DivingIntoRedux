e3b005049b068e76cd7e8e79ff230777f6358409
HamidrezaBahmani <hb.hamidrezabahmani.com@gmail.com>
2023-11-28 16:52:06 +0330

    Migrating Everything To Redux Toolkit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,45 +1,34 @@
 import classes from "./Counter.module.css";
-//13-utelize store in Counter
-//allow us to automatically select a part of our state managed by the store
-//17-for dispatching we should use useDispatch hook
 import { useSelector, useDispatch } from "react-redux";
+import { counterActions } from "../store/index";
 const Counter = () => {
-  //18-call useDispatch we don't pass any argument but it returns
-  //dispatch func
   const dispatch = useDispatch();
 
-  //19-decalre to handler to wireup with buttuns
   const increamtHandler = () => {
-    //20-use dispatch to dispatch new action
-    //21-a action is obj with type property
-    dispatch({ type: "increament" });
+    dispatch(counterActions.increment());
   };
 
   const increaseHandler = () => {
-    dispatch({ type: "increase", amount: 5 });
+    dispatch(counterActions.increase(10));
   };
 
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
 
-  //14-we need to pass finction to use selector (basically to detemine which piece of data we need to extract from store)
-  //for i.e we want to extract state=>state.counter
-  //redux automatically setup the subscrition for store for this component
   const counter = useSelector((state) => state.counter);
   const show = useSelector((state) => state.showCounter);
   const toggleCounterHandler = () => {
-    dispatch({ type: "toggle" });
+    dispatch(counterActions.toggleCounter());
   };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      {/* 15-use counter */}
+
       {show && <div className={classes.value}>{counter}</div>}
-      {/* 16- for dipatching action first setup the button */}
+
       <div>
-        {/* 22-wire up functioms with buttons*/}
         <button onClick={increamtHandler}>Increment</button>
         <button onClick={increaseHandler}>Increase By 5</button>
         <button onClick={decrementHandler}>Decrement</button>
